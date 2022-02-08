420aeab62a291de91047da03afeccc6980ea8856
Tiago de Lima Batista <tigolimab223@gmail.com>
2022-02-08 16:18:19 -0300

    Versão final

--- scripts.js
+++ scripts.js
@@ -3,25 +3,23 @@ const listaCartas = [];
 const imagens = ["bobrossparrot", "fiestaparrot", "tripletsparrot",
     "explodyparrot", "metalparrot", "revertitparrot", "unicornparrot"]
 
-let trancar = false;
-let carta1, carta2;
-const carta = document.querySelectorAll(".carta");
+const numeroCartas = parseInt(prompt("Quantos pares de cartas você quer? (Mínimo de 4 e máximo de 14)"));
+//confirma se o numero inserido pelo usuario é par ou não
+if (numeroCartas % 2 !== 0 || numeroCartas <= 2 || numeroCartas > 14) {
+    alert("Por favor, insira um par válido!")
+    numeroCartas = null
+}
 
 function cards() {
-    const numeroCartas = parseInt(prompt("Quantos pares de cartas você quer? (Mínimo de 4 e máximo de 14)"));
-    //confirma se o numero inserido pelo usuario é par ou não
-    if (numeroCartas % 2 !== 0 || numeroCartas <= 2 || numeroCartas > 14) {
-        alert("Por favor, insira um par válido!")
-        numeroCartas = null
-    }
-
 
     for (let i = 0; i < numeroCartas; i++) {
         let carta = `
-        <div class="carta" data-carta="${imagens[i]}">
+        <div class="carta" data-carta="${imagens[i]}" data-identifier="card">
+            <figure class="gif">
+            <img class="back-face face" src="imagens/${imagens[i]}.gif data-identifier="back-face">
+            </figure>
             <figure>
-            <img class="back-face" src="imagens/${imagens[i]}.gif" disabled>
-            <img class="front-face" src="imagens/front.png">
+            <img class="front-face face" src="imagens/front.png"data-identifier="front-face">
             </figure>
         </div>
         `
@@ -35,6 +33,7 @@ function cards() {
     }
 }
 
+//função que ao clickar na carta ela girar
 function flipCard() {
     if (trancar) return false;
     console.log(this)
@@ -45,14 +44,18 @@ function flipCard() {
     }
     carta2 = this
     checarCarta();
-}
 
+    jogadas++;
+}
+//checa a combinação
 function checarCarta() {
     let checar = carta1.dataset.carta === carta2.dataset.carta
     console.log(checar)
 
     !checar ? fixarCartas() : resetarCartas(checar);
 }
+
+//fixa as cartas para as outras não serem clickadas
 function fixarCartas() {
     trancar = true
     setTimeout(() => {
@@ -63,21 +66,44 @@ function fixarCartas() {
     }, 1000);
 
 }
-
+//reseta a carta caso erre a combinação
 function resetarCartas(checar = false) {
     if (checar) {
         carta1.removeEventListener('click', flipCard)
         carta2.removeEventListener('click', flipCard)
+        carta1.classList.add(fimDeJogo++)
+        carta2.classList.add(fimDeJogo++)
+        setTimeout(Acabou, 500)
     }
     [carta1, carta2, trancar] = [null, null, false]
 }
 
-//
-//função para embaralhar as cartas
-//função que ao clickar na carta ela girar (transition-transform)
-//fazer a logica de combinar as cartas
 //alerta dizendo em quantas jogadas a pessoa ganhou
-//implementar o contador
+function Acabou() {
+    if (fimDeJogo == numeroCartas) {
+        alert("Você ganhou em " + jogadas + " jogadas!")
+    }
+    else {
+        console.log("teste")
+    }
+
+}
+
+
+//função para embaralhar as cartas
+imagens.sort(comparador);
+function comparador() {
+    return Math.random() - 0.5;
+}
+
 cards()
+
+// Esta função pode ficar separada do código acima, onde você preferir
+let fimDeJogo = 0;
+let jogadas = 0;
+let trancar = false;
+let carta1, carta2;
+const carta = document.querySelectorAll(".carta");
 const cartaFrente = document.querySelectorAll("img")
 carta.forEach(carta => carta.addEventListener("click", flipCard))
+
